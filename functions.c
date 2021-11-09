5b734b6518b6834df970d97decfbc86bf18870b1
Steven <deltaorb637@gmail.com>
2021-11-08 21:10:22 -0500

    finished

--- functions.c
+++ functions.c
@@ -70,7 +70,7 @@ struct pop_entry * read_data(char * PATH){
 	//get size
 	struct stat info;
 	stat(PATH, &info);
-	struct pop_entry * e_arr = malloc(info.st_size / sizeof(struct pop_entry));
+	struct pop_entry * e_arr = calloc(info.st_size / sizeof(struct pop_entry), sizeof(struct pop_entry));
 	
 	//reading file
 	int read_in = read(in_fd, e_arr, info.st_size);
@@ -85,11 +85,11 @@ struct pop_entry * read_data(char * PATH){
 
 void add_data (char * PATH){
 	char DATA[256];
-	printf("Enter in data entry in the format of year borough population\n");
+	printf("Enter in data entry in the format of year, borough, population (please include commas)\n");
 	
 	struct pop_entry e;
 	fgets(DATA, 256, stdin);
-	sscanf(DATA, "%d %s %d", &e.year, e.boro, &e.pop);
+	sscanf(DATA, "%d, %[^,], %d", &e.year, e.boro, &e.pop);
 	int in_fd = open(PATH, O_WRONLY || O_APPEND | O_CREAT, 0644);
 	lseek(in_fd, 0, SEEK_END);
 	write(in_fd, &e, sizeof(struct pop_entry));
@@ -99,13 +99,25 @@ void add_data (char * PATH){
 
 void update_data(char * PATH){
 	struct pop_entry *entries = read_data(PATH);
-	char DATA[10];
+	
+	struct pop_entry temp;
+
+	char ENTRY[10];
 	printf("\nEnter data entry number to update\n");
 	
-	fgets(DATA, 10, stdin);
-	// int entry;
+	fgets(ENTRY, 10, stdin);
+	int entry;
+	sscanf(ENTRY, "%d", &entry);
 
-	// sscanf(DATA, "%d", &entry);
-	printf("Updated entry %d with", 1);
+	printf("\nEnter in data entry in the format of year, borough, population (please include commas)\n");
+	char DATA [256];
+	fgets(DATA, 256, stdin);
+	sscanf(DATA, "%d, %[^,], %d", &temp.year, temp.boro, &temp.pop);
+	
+	int fd = open(PATH, O_WRONLY);
+	lseek(fd, entry * sizeof(struct pop_entry),  SEEK_SET);
+	int w = write(fd, &temp, sizeof(struct pop_entry));
+	close(fd);
+	printf("Updated entry %d with [%d %s %d] to file %s\n", entry, temp.year, temp.boro, temp.pop, PATH);
 	
 }
